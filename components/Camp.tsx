93e6315226eb932fb19019ec3abd0c31c0396b42
Sid20rathi <98732630+Sid20rathi@users.noreply.github.com>
2024-08-11 15:58:49 +0530

    Update Camp.tsx

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -15,7 +15,9 @@ interface CampProps{
 
 const Campsite =({backgroundImage,title}:CampProps)=>{
   return(
-    <div className = {` object-none    h-full w-full bg-cover  ${backgroundImage}  bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl` } >
+    <div className="size-20">
+         <div className = {` object-cover   h-full w-11/12 bg-cover  ${backgroundImage}  bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl absolute overflow-clip sm:rounded-5xl md:rounded-5xl
+    ` } >
       <div className = "flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10 ">
         <div className = "flexCenter gap-4">
           <div className = "rounded-full bg-green-50 p-4">
@@ -35,6 +37,8 @@ const Campsite =({backgroundImage,title}:CampProps)=>{
 
       </div>
     </div>
+    </div>
+ 
 
 
   )  
@@ -43,17 +47,17 @@ const Campsite =({backgroundImage,title}:CampProps)=>{
 const Camp = () => {
   return (
     <section className = "h-full  2xl:max-conatiner relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20"> 
-     <div className=" flex  flex-row gap-y-5 h-[340px] w-full items-start justify-start  lg:h-[400px] xl:h-[640px] ">
+     <div className=" flex  flex-row gap-y-5 h-[340px] w-full items-start justify-start sm:h-[200px] lg:h-[400px] xl:h-[640px] sm:rounded-md ">
       <Campsite
         backgroundImage = "bg-bg-img-1"
-        title ="Kedarkantha camp "/>
+        title ="Kedarkantha camp " />
 
         
 
 
                                                                     
      </div>
-     <div className =" flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+     <div className =" flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6 xl:pt-80 xl:pr-7">
       <div className = " bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative overflow-hiddden rounded-3xl">
        <h2 className = "regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
         <strong>
