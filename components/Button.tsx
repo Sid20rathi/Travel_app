fdf6228b22ca52c2b6d5a8857fedaf8217ea2f7c
Sid20rathi <98732630+Sid20rathi@users.noreply.github.com>
2024-08-11 15:30:34 +0530

    Update Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,12 +3,14 @@ interface ButtonProps{
     type : 'button' ,
     title : string,
     icon ?: string,
-    variant : 'btn_dark_green'
-}
+    variant : 'btn_dark_green',
+    link ?: string ,
+    onfun ?: ()=> void
+};
 
-export const Button = ({type,title,icon,variant}:ButtonProps)=>{
+export const Button = ({type,title,icon,variant,link,onfun} :ButtonProps)=>{
     return(
-       <button type = {type} className = {`flexCenter gap-3 rounded-full p-2 border bg-slate-300 ${variant}`}>
+       <button type = {type} className = {`flexCenter gap-3 rounded-full p-2 border bg-slate-300 ${variant}`} onClick={onfun}>
         <label className ="bold-16 whitespace-nowrap ">{title}</label>
 
 
