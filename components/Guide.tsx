192b7d15071a616a0ff708eebf51030a8116b5a1
Sid20rathi <98732630+Sid20rathi@users.noreply.github.com>
2024-08-11 16:02:42 +0530

    Update Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Guide = () => {
   return (
-    <section className="flexCenter flex-col">
+    <section className="flexCenter flex-col pt-20">
       <div className="padding-container max-container w-full pb-24 ">
         <Image src="/camp.svg" alt="camp" width={50} height={50} />
         <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
